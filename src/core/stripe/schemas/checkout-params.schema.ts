1e42b17fc60ae6a14157ad285ee9b8a05e2559aa
Gianluca Nativo <giannativo@yahoo.com.ar>
2023-07-26 13:14:55 -0300

    Add validations for dependencies among fields, remove old validators and adding extra test requests

--- src/core/stripe/schemas/checkout-params.schema.ts
+++ src/core/stripe/schemas/checkout-params.schema.ts
@@ -18,9 +18,24 @@ export const CheckoutParamsSchema = z.object({
     email: z.optional(z.string().email()),
     lineItems: z.array(LineItemSchema).nonempty(),
     paymentMethod: PaymentModesSchema,
-    createCustomer: z.boolean(),
-}).refine(schema => 
-    (schema.customerId && schema.email) ? false : true, 
-{
-    message: 'Data must have either a customerId or an email, not both.'
+    createCustomer: z.optional(z.boolean()),
+}).superRefine((schema, ctx) => {
+    if (schema.customerId && schema.email) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Data must have either a customerId or an email, not both.',
+      });
+    }
+    if (schema.customerId && schema.createCustomer !== undefined) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'CreateCustomer should not be defined if a customerId is sent.',
+        });
+      }
+    if (schema.customerId === undefined && schema.createCustomer === undefined) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'CreateCustomer must be defined if a customerId is not sent.',
+      });
+    }
 });
